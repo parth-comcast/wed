bb1cba47fa647bbe82fccf98d8d28d5d534a869f
parth2973 <parth.patel1496@gmail.com>
2018-07-28 18:58:06 -0400

    Seperate nav component

--- src/app/module.ts
+++ src/app/module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from './shared/module';
 
 import { LandingComponent } from './landing/landing';
+import { NavComponent } from './nav/nav';
 import { HeaderComponent } from './header/header';
 import { AboutComponent } from './about/about';
 
 @NgModule({
   declarations: [
     LandingComponent,
+    NavComponent,
     HeaderComponent,
     AboutComponent
   ],
