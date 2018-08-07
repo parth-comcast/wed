dd4cf9d13e8408f77c5229b30cd1a4743bd3e2b8
parth2973 <parth.patel1496@gmail.com>
2018-08-07 09:17:12 -0400

    nav design cont.

--- src/app/nav/nav.ts
+++ src/app/nav/nav.ts
@@ -1,9 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.html',
   styleUrls: ['./nav.scss']
 })
-export class NavComponent {
+export class NavComponent implements OnInit {
+
+  public links = [
+    {
+        label: 'Home',
+        link: 'home'
+    },
+    {
+        label: 'Love Story',
+        link: 'love-story'
+    },
+    {
+        label: 'Gallery',
+        link: 'gallery'
+    },
+    {
+        label: 'Venue',
+        link: 'venue'
+    }
+];
+
+  public ngOnInit(): void { }
 }
